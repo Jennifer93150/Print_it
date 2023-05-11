7e7c9810d5f26416b36a395e9ceb3a7ce6b701b0
Jennifer93150 <73002210+Jennifer93150@users.noreply.github.com>
2023-05-11 17:24:42 +0200

    first dot in white

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,7 @@ const prevImage = document.querySelector('arrow_left');
 const nextImage = document.querySelector('arrow_right');
 let count = 0;
 
-// Ajout des dots
+// Ajout des dots en fonction du nombre d'images
 for(var i=0; i < nbImages; i++)
 {
 	var newDiv = document.createElement("div");
@@ -31,31 +31,51 @@ for(var i=0; i < nbImages; i++)
 	newDiv.classList.add("dot");
 }
 
+// Selection des dots 
 let arrayDots = document.querySelectorAll('.dot');
+
+// Premier dot en blanc
+if(count === 0 && !arrayDots[count].classList.contains('dot_selected')){
+	arrayDots[count].classList.add('dot_selected');
+}
 
 // Evènement sur la flèche précédente
 document.getElementById("prev").addEventListener("click", function() {
+	// Suppression class dot_selected
 	arrayDots[count].classList.remove('dot_selected');
+	//si nbr count est sup à 0
 	if(count > 0){
+		//Décrémente
 		count--;
 	}else{
+		//Sinon count sera = nbr image -1
 		count = nbImages - 1;
 	}
+	// Ajout class dot_selected
 	arrayDots[count].classList.add('dot_selected');
+	// Ajout src image en fonction du nbr de l'image
 	document.getElementById("image").setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
+	// Ajout du texte 
 	document.getElementById("text-on-image").innerHTML = slides[count].tagLine;
 });
 
 // Evènement sur la flèche suivante
 document.getElementById("next").addEventListener("click", function() {
+	// Suppression class dot_selected
 	arrayDots[count].classList.remove('dot_selected');
+	//si nbr count est inf au nbr image -1
 	if(count < nbImages - 1 ){
+		// Incrémente
 		count++;
 	}else{
+		//Sinon count sera = 0
 		count = 0;
 	}
+	// Ajout class dot_selected
 	arrayDots[count].classList.add('dot_selected');
+	// Ajout src image en fonction du nbr de l'image
 	document.getElementById("image").setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
+	// Ajout du texte 
 	document.getElementById("text-on-image").innerHTML = slides[count].tagLine;
 });
 
